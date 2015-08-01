f72e48201109d75dcd26c2df0ae885433a0c4460
Thiago Akio Nakamura <akionakas@gmail.com>
2015-07-31 23:53:42 -0300

    Logger configuration

--- sklearn/mixture/tests/test_mppca.py
+++ sklearn/mixture/tests/test_mppca.py
@@ -1,6 +1,7 @@
 import unittest
 import copy
 import sys
+import logging
 
 from nose.tools import assert_true
 import numpy as np
@@ -13,3 +14,26 @@ from sklearn.utils.testing import assert_greater
 from sklearn.utils.testing import assert_raise_message
 from sklearn.metrics.cluster import adjusted_rand_score
 from sklearn.externals.six.moves import cStringIO as StringIO
+
+# set up logging to file - see previous section for more details
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M')
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+# tell the handler to use this format
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
+
+def test_dummy():
+    # This is a dummy test
+    var1 = 1
+    var2 = 2
+
+    result = var1 + var2
+    assert_true(result == 3)
+    logging.info('DummyTest: Result should be three: ' + str(result))
