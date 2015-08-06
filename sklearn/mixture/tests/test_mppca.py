293836636c19e6b07abe786c5d4889bf08d0de49
Thiago Akio Nakamura <akionakas@gmail.com>
2015-08-05 23:01:02 -0300

    First few real test

--- sklearn/mixture/tests/test_mppca.py
+++ sklearn/mixture/tests/test_mppca.py
@@ -34,16 +34,44 @@ def test_dummy():
     # This is a dummy test
     var1 = 1
     var2 = 2
-
     result = var1 + var2
     assert_true(result == 3)
     logging.info('DummyTest: Result should be three: ' + str(result))
+    logging.info('DummyTest: OK')
 
 
-def test_create_mppca():
-    model = mixture.MPPCA(n_components=1, n_pc=1,
-                 random_state=None, tol=1e-3, min_covar=1e-3,
-                 n_iter=100, n_init=1, params='wmpn', init_params='wmpn',
-                 verbose=0)
-    assert_true(model.n_pc == 1)
-    logging.info('DummyTest: Result should be one: ' + str(model.n_pc))
+def test_default_create_mppca():
+    model = mixture.MPPCA()
+    assert_true(model.n_components == 1, msg='Wrong n_components')
+    assert_true(model.n_pc == 1, msg='Wrong n_pc')
+    assert_true(model.tol == 1e-3, msg='Wrong tol')
+    assert_true(model.min_covar == 1e-3, msg='Wrong min_covar')
+    assert_true(model.random_state == None, msg='Wrong random_state')
+    assert_true(model.n_iter == 100, msg='Wrong n_iter')
+    assert_true(model.n_init == 1, msg='Wrong n_init')
+    assert_true(model.params == 'wmpn', msg='Wrong params')
+    assert_true(model.init_params == 'wmpn', msg='Wrong init_params')
+    assert_true(model.verbose == 0, msg='Wrong verbose')
+    assert_true(model.weights_.shape == (1,), msg='Wrong weights_')
+    assert_true(~model.converged_, msg='Wrong converged_')
+    logging.info('DefaultCreationTest: OK')
+
+
+def test_custom_create_mppca():
+    model = mixture.MPPCA(n_components=2, n_pc=2,
+                          random_state=1, tol=1e-6, min_covar=1e-6,
+                          n_iter=10, n_init=2, params='w', init_params='m',
+                          verbose=2)
+    assert_true(model.n_components == 2, msg='Wrong n_components')
+    assert_true(model.n_pc == 2, msg='Wrong n_pc')
+    assert_true(model.tol == 1e-6, msg='Wrong tol')
+    assert_true(model.min_covar == 1e-6, msg='Wrong min_covar')
+    assert_true(model.random_state == 1, msg='Wrong random_state')
+    assert_true(model.n_iter == 10, msg='Wrong n_iter')
+    assert_true(model.n_init == 2, msg='Wrong n_init')
+    assert_true(model.params == 'w', msg='Wrong params')
+    assert_true(model.init_params == 'm', msg='Wrong init_params')
+    assert_true(model.verbose == 2, msg='Wrong verbose')
+    assert_true(model.weights_.shape == (2,), msg='Wrong weights_')
+    assert_true(~model.converged_, msg='Wrong converged_')
+    logging.info('CustomCreationTest: OK')
