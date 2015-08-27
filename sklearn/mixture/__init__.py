966ef93921387cf3fb52d81f66d84c9510d3a83c
Thiago Akio Nakamura <akionakas@gmail.com>
2015-08-26 23:59:37 -0300

    Added PGMM to init

--- sklearn/mixture/__init__.py
+++ sklearn/mixture/__init__.py
@@ -7,11 +7,13 @@ from .gmm import GMM, distribute_covar_matrix_to_match_covariance_type
 from .gmm import _validate_covars
 from .dpgmm import DPGMM, VBGMM
 from .mppca import MPPCA
+from .pgmm import PGMM
 
 __all__ = ['DPGMM',
            'GMM',
            'VBGMM',
            'MPPCA',
+           'PGMM',
            '_validate_covars',
            'distribute_covar_matrix_to_match_covariance_type',
            'log_multivariate_normal_density',
